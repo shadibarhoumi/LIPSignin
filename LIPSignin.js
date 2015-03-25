9c7cb86def5e45dde8b0278dc11450d9f2ff14f2
Shadi Barhoumi <shadibarhoumi@gmail.com>
2015-03-24 19:15:10 -0700

    sign in and visit counting

--- LIPSignin.js
+++ LIPSignin.js
@@ -1,17 +1,39 @@
-if (Meteor.isClient) {
-  // counter starts at 0
-  Session.setDefault("counter", 0);
+Visitors = new Mongo.Collection('visitors');
 
-  Template.hello.helpers({
-    counter: function () {
-      return Session.get("counter");
+if (Meteor.isClient) {
+  Template.signin.helpers({
+    vistor: function () {
+      return Visitors.find();
     }
   });
 
-  Template.hello.events({
-    'click button': function () {
-      // increment the counter when button is clicked
-      Session.set("counter", Session.get("counter") + 1);
+  Template.signin.events({
+    'click .signinButton, keyup .email': function(e) {
+      if ((e.type === 'click') || (e.type === 'keyup' && e.which === 13) ) {
+        var email = $('.email').val();
+        // insert visitor into database
+        var matches = Visitors.find({email: email}).fetch();
+        Visitors.update
+        if (matches.length) { // if user exists
+          var visitor = matches[0];
+          console.log("found vis: ", visitor);
+          // increment number of visits
+          Visitors.update(visitor._id, {$inc: {numVisits: 1}});
+          visitor = Visitors.find(visitor._id).fetch()[0];
+          console.log("updated visitor: ", visitor);
+          $(".signinForm").prepend($("</p>").text("Welcome back, " + visitor.email + "! This is visit #" + visitor.numVisits + " for you!"));
+        } else {
+          // create new user
+          Visitors.insert({ 
+            email: email,
+            numVisits: 1,
+            createdAt: new Date()
+          });
+          $(".signinForm").prepend($("</p>").text("Welcome, " + email + "! This is your first visit."));
+        }
+
+        $('.email').val('');
+      }
     }
   });
 }
